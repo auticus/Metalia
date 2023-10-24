d6aeeaddfac53edb6b029f27b5537f83c98802d9
Christopher Nye <icedcrow@yahoo.com>
2023-10-24 13:13:14 -0700

    Coding for the Player Controller.

--- Source/Metalia/Private/Player/MetaliaPlayerController.cpp
+++ Source/Metalia/Private/Player/MetaliaPlayerController.cpp
@@ -2,4 +2,27 @@
 
 
 #include "Player/MetaliaPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AMetaliaPlayerController::AMetaliaPlayerController()
+{
+	bReplicates = true;
+}
+
+void AMetaliaPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(MetaliaPlayerContext);
+
+	UEnhancedInputLocalPlayerSubsystem* localPlayerSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(localPlayerSubsystem);
+	localPlayerSubsystem->AddMappingContext(MetaliaPlayerContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI inputModeData;
+	inputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	inputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(inputModeData);
+}
--- Source/Metalia/Public/Player/MetaliaPlayerController.h
+++ Source/Metalia/Public/Player/MetaliaPlayerController.h
@@ -6,12 +6,24 @@
 #include "GameFramework/PlayerController.h"
 #include "MetaliaPlayerController.generated.h"
 
+class UInputMappingContext;
+
 /**
- * 
+ * Main player controller.
  */
 UCLASS()
 class METALIA_API AMetaliaPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AMetaliaPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> MetaliaPlayerContext;
 };
