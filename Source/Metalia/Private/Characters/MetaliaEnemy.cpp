45bd712fab2d79e7a66bf438df28f496d645f3a3
Christopher Nye <icedcrow@yahoo.com>
2023-10-25 21:35:20 -0700

    Add enemy highlighting with stencil.

--- Source/Metalia/Private/Characters/MetaliaEnemy.cpp
+++ Source/Metalia/Private/Characters/MetaliaEnemy.cpp
@@ -2,13 +2,20 @@
 
 
 #include "Characters/MetaliaEnemy.h"
+#include "Metalia/Metalia.h"
 
 void AMetaliaEnemy::HighlightActor()
 {
 	bHighlighted = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_STENCIL_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_STENCIL_RED);
 }
 
 void AMetaliaEnemy::UnhighlightActor()
 {
 	bHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
