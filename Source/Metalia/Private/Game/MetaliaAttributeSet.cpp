f1a4c62210ab7cea863f5552f25f25d2b1aaca7d
Christopher Nye <icedcrow@yahoo.com>
2023-10-31 07:48:21 -0700

    Attribute set replication.

--- Source/Metalia/Private/Game/MetaliaAttributeSet.cpp
+++ Source/Metalia/Private/Game/MetaliaAttributeSet.cpp
@@ -2,4 +2,42 @@
 
 
 #include "Game/MetaliaAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UMetaliaAttributeSet::UMetaliaAttributeSet()
+{
+}
+
+void UMetaliaAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	/* This is the area where we register our variables for replication*/
+	/* Look up the various conditions and when to notify.  In this case we will always notify so that
+	*  we can get notified by server when any attempt came to change it, even if the values are the same */
+	DOREPLIFETIME_CONDITION_NOTIFY(UMetaliaAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMetaliaAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMetaliaAttributeSet, Metal, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMetaliaAttributeSet, MaxMetal, COND_None, REPNOTIFY_Always);
+}
+
+void UMetaliaAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMetaliaAttributeSet, Health, OldHealth);
+}
+
+void UMetaliaAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMetaliaAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UMetaliaAttributeSet::OnRep_Metal(const FGameplayAttributeData& OldMetal) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMetaliaAttributeSet, Metal, OldMetal);
+}
+
+void UMetaliaAttributeSet::OnRep_MaxMetal(const FGameplayAttributeData& OldMaxMetal) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMetaliaAttributeSet, MaxMetal, OldMaxMetal);
+}
--- Source/Metalia/Public/Game/MetaliaAttributeSet.h
+++ Source/Metalia/Public/Game/MetaliaAttributeSet.h
@@ -7,11 +7,39 @@
 #include "MetaliaAttributeSet.generated.h"
 
 /**
- * 
+ * Attribute set for Metalia characters.
  */
 UCLASS()
 class METALIA_API UMetaliaAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Primary Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Primary Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Metal, Category = "Primary Attributes")
+	FGameplayAttributeData Metal;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMetal, Category = "Primary Attributes")
+	FGameplayAttributeData MaxMetal;
+
+public:
+	UMetaliaAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Metal(const FGameplayAttributeData& OldMetal) const;
+
+	UFUNCTION()
+	void OnRep_MaxMetal(const FGameplayAttributeData& OldMaxMetal) const;
 };
