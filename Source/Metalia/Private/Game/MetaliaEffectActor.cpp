d51a85ae6ac9f6d6f348d23eff3f54d752e2a719
Christopher Nye <icedcrow@yahoo.com>
2023-11-06 21:32:07 -0700

    Rework the Effect Actor

--- Source/Metalia/Private/Game/MetaliaEffectActor.cpp
+++ Source/Metalia/Private/Game/MetaliaEffectActor.cpp
@@ -2,58 +2,33 @@
 
 
 #include "Game/MetaliaEffectActor.h"
-#include "Components/SphereComponent.h"
-#include <AbilitySystemInterface.h>
-#include <Game/MetaliaAttributeSet.h>
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
 // Sets default values
 AMetaliaEffectActor::AMetaliaEffectActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-	EffectMesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(EffectMesh);
-
-	EffectSphere = CreateDefaultSubobject<USphereComponent>("EffectSphere");
-	EffectSphere->SetupAttachment(GetRootComponent());
-
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
 // Called when the game starts or when spawned
 void AMetaliaEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	EffectSphere->OnComponentBeginOverlap.AddDynamic(this, &AMetaliaEffectActor::OnOverlap);
-	EffectSphere->OnComponentEndOverlap.AddDynamic(this, &AMetaliaEffectActor::EndOverlap);
 }
 
-void AMetaliaEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, 
-	AActor* OtherActor, 
-	UPrimitiveComponent* OtherComp, 
-	int32 OtherBodyIndex, 
-	bool bFromSweep, 
-	const FHitResult& SweepResult)
+void AMetaliaEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
-	/* ATTEMPTING TO READ THE ATTRIBUTE RESULTS IN ACCESS VIOLATION CRASH*/
+	UAbilitySystemComponent* TargetSystem = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	if (TargetSystem == nullptr) return;
 
-	// the below is not the best way to do this - we have better ways we just dont know yet
-	if (IAbilitySystemInterface* otherAbilitySystem = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UMetaliaAttributeSet* MetaliaAttributeSet = Cast<UMetaliaAttributeSet>(
-			otherAbilitySystem->GetAbilitySystemComponent()->GetAttributeSet(UMetaliaAttributeSet::StaticClass()));
-		
-		// do the thing we shouldn't do and remove the const so we can change things directly
-		UMetaliaAttributeSet* attributeSet = const_cast<UMetaliaAttributeSet*>(MetaliaAttributeSet);
-		float currentHealth = MetaliaAttributeSet->GetHealth();
-		attributeSet->SetHealth(currentHealth + 25.f);
-	}
-
-	Destroy();
+	check(GameplayEffectClass);
+	float AbilityLevel = 1.f;
+	FGameplayEffectContextHandle EffectContextHandle = TargetSystem->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	FGameplayEffectSpecHandle EffectSpecHandle = TargetSystem->MakeOutgoingSpec(GameplayEffectClass, AbilityLevel, EffectContextHandle);
+	TargetSystem->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); // the * dereferences this to the object
 }
-
-void AMetaliaEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-
-}
-
--- Source/Metalia/Public/Game/MetaliaEffectActor.h
+++ Source/Metalia/Public/Game/MetaliaEffectActor.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "MetaliaEffectActor.generated.h"
 
-class USphereComponent;
+class UGameplayEffect;
 
 UCLASS()
 class METALIA_API AMetaliaEffectActor : public AActor
@@ -17,26 +17,16 @@ public:
 	// Sets default values for this actor's properties
 	AMetaliaEffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent,
-		AActor* OtherActor,
-		UPrimitiveComponent* OtherComp,
-		int32 OtherBodyIndex,
-		bool bFromSweep,
-		const FHitResult& SweepResult);
-
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+protected:
+	UPROPERTY(EditAnywhere, Category="Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+
 private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> EffectMesh;
-
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> EffectSphere;
-
 };
