a385a483aa3c6a36cdac46ef0b04a70d574a325e
Christopher Nye <icedcrow@yahoo.com>
2023-11-13 19:34:00 -0700

    Implement an infinite fire effect damage area.

--- Source/Metalia/Private/Game/MetaliaEffectActor.cpp
+++ Source/Metalia/Private/Game/MetaliaEffectActor.cpp
@@ -5,6 +5,7 @@
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemInterface.h"
 #include "AbilitySystemBlueprintLibrary.h"
+#include "GameplayEffectTypes.h"
 
 // Sets default values
 AMetaliaEffectActor::AMetaliaEffectActor()
@@ -31,5 +32,69 @@ void AMetaliaEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<U
 	FGameplayEffectContextHandle EffectContextHandle = TargetSystem->MakeEffectContext();
 	EffectContextHandle.AddSourceObject(this);
 	FGameplayEffectSpecHandle EffectSpecHandle = TargetSystem->MakeOutgoingSpec(GameplayEffectClass, AbilityLevel, EffectContextHandle);
-	TargetSystem->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); // the * dereferences this to the object
+	const FActiveGameplayEffectHandle EffectHandle = TargetSystem->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); // the * dereferences this to the object
+
+	const bool bIsInfinite = EffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	if (bIsInfinite && InfiniteGameplayApplicationEndPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		ActiveEffectHandles.Add(EffectHandle, TargetSystem);
+	}
+}
+
+void AMetaliaEffectActor::OnOverlap(AActor* TargetActor)
+{
+	if (InstantGameplayEffectClass && InstantGameplayApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	if (DurationGameplayEffectClass && DurationGameplayApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	if (InfiniteGameplayEffectClass && InfiniteGameplayApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+}
+
+void AMetaliaEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if (InstantGameplayEffectClass && InstantGameplayApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	if (DurationGameplayEffectClass && DurationGameplayApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	if (InfiniteGameplayEffectClass && InfiniteGameplayApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+	if (InfiniteGameplayApplicationEndPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		RemoveInfiniteEffects(TargetActor);
+	}
+}
+
+void AMetaliaEffectActor::RemoveInfiniteEffects(AActor* TargetActor)
+{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (!IsValid(TargetASC)) return;
+
+	TArray<FActiveGameplayEffectHandle> HandlesToTerminate;
+	for (auto HandledPair : ActiveEffectHandles)
+	{
+		if (TargetASC == HandledPair.Value)
+		{
+			const int StacksToRemove = 1;
+			TargetASC->RemoveActiveGameplayEffect(HandledPair.Key, StacksToRemove);
+			HandlesToTerminate.Add(HandledPair.Key);
+		}
+	}
+
+	for (auto HastaLavista : HandlesToTerminate)
+	{
+		ActiveEffectHandles.FindAndRemoveChecked(HastaLavista);
+	}
 }
--- Source/Metalia/Public/Game/MetaliaEffectActor.h
+++ Source/Metalia/Public/Game/MetaliaEffectActor.h
@@ -4,9 +4,27 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameplayEffectTypes.h"
 #include "MetaliaEffectActor.generated.h"
 
 class UGameplayEffect;
+class UAbilitySystemComponent;
+struct FActiveGameplayEffectHandle;
+
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy
+{
+	ApplyOnOverlap,
+	ApplyOnEndOverlap,
+	DoNotApply
+};
+
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy
+{
+	RemoveOnEndOverlap,
+	DoNotRemove
+};
 
 UCLASS()
 class METALIA_API AMetaliaEffectActor : public AActor
@@ -18,11 +36,31 @@ public:
 	AMetaliaEffectActor();
 
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadonly, Category="Effects")
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Effects")
+	bool bDestroyOnEffectRemoval;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
 	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Effects")
+	EEffectApplicationPolicy InstantGameplayApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Effects")
 	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Effects")
+	EEffectApplicationPolicy DurationGameplayApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Effects")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Effects")
+	EEffectApplicationPolicy InfiniteGameplayApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Effects")
+	EEffectRemovalPolicy InfiniteGameplayApplicationEndPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
+
+	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveEffectHandles;
 
 protected:
 	// Called when the game starts or when spawned
@@ -31,5 +69,12 @@ protected:
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
+	UFUNCTION(BlueprintCallable)
+	void OnOverlap(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
+
 private:
+	void RemoveInfiniteEffects(AActor* TargetActor);
 };
