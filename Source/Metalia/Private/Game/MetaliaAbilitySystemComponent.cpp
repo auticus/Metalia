2b8912bd4b7b16e23eb4e17bf56879d8e4298003
Christopher Nye <icedcrow@yahoo.com>
2023-12-05 16:55:31 -0700

    55 Get All Asset Tags

--- Source/Metalia/Private/Game/MetaliaAbilitySystemComponent.cpp
+++ Source/Metalia/Private/Game/MetaliaAbilitySystemComponent.cpp
@@ -16,5 +16,11 @@ void UMetaliaAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Abil
 													const FGameplayEffectSpec& EffectSpec, 
 													FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("Effect applied!"));
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, Msg);
+	}
 }
