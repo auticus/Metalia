b84330a8cc52fb759361a3d9ade038c35a1a2f68
Christopher Nye <icedcrow@yahoo.com>
2024-01-24 19:16:03 -0700

    93 - Attribute Info Delegate
    At this point the attribute names are mapped to the data table and will all say "Strength" as will the values all reflect the strength value.

--- Source/Metalia/Private/UI/Controllers/AttributeMenuWidgetController.cpp
+++ Source/Metalia/Private/UI/Controllers/AttributeMenuWidgetController.cpp
@@ -2,10 +2,18 @@
 
 
 #include "UI/Controllers/AttributeMenuWidgetController.h"
+#include "MetaliaGameplayTags.h"
+#include <Game/MetaliaAttributeSet.h>
+#include "Game/AttributeInfo.h"
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
-	Super::BroadcastInitialValues();
+	UMetaliaAttributeSet* AS = CastChecked<UMetaliaAttributeSet>(AttributeSet);
+	check(AttributeInfo);
+
+	FMetaliaAttributeInfo Info = AttributeInfo->FindAttributeInfoFromTag(FMetaliaGameplayTags::Get().Attributes_Primary_Strength, true);
+	Info.AttributeValue = AS->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
--- Source/Metalia/Public/UI/Controllers/AttributeMenuWidgetController.h
+++ Source/Metalia/Public/UI/Controllers/AttributeMenuWidgetController.h
@@ -6,6 +6,11 @@
 #include "UI/Controllers/MetaliaWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+struct FMetaliaAttributeInfo;
+class UAttributeInfo;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FMetaliaAttributeInfo&, Info);
+
 /**
  * 
  */
@@ -15,7 +20,16 @@ class METALIA_API UAttributeMenuWidgetController : public UMetaliaWidgetControll
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FAttributeInfoSignature AttributeInfoDelegate;
+
+	UFUNCTION(BlueprintCallable)
 	virtual void BroadcastInitialValues() override;
 	virtual void BindCallbacksToDependencies() override;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UAttributeInfo> AttributeInfo;
 	
 };
