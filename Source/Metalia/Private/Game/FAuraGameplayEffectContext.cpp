17bedf650b911e8fc7220f6198107860bdea5ad8
Christopher Nye <icedcrow@yahoo.com>
2024-02-28 09:49:34 -0700

    150 - beginning of custom gameplay effect context

---
+++ Source/Metalia/Private/Game/FAuraGameplayEffectContext.cpp
@@ -0,0 +1,8 @@
+// Copyright Auticus Studios
+
+#include "Game/FAuraGameplayEffectContext.h"
+
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	return false;
+}
